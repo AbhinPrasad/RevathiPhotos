f3b8f80451deff7cc79581973a7fbfe8050f2fa7
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-02-26 12:49:40 +0530

    initial folder structure

--- revathi_studio_client/src/App.jsx
+++ revathi_studio_client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home, About, Contact, Gallery, Auth } from "./pages";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/gallery" element={<Gallery />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/auth" element={<Auth />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
-export default App
+export default App;
---
+++ revathi_studio_client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Gallery } from "./Gallery";
+export { default as Contact } from "./Contact";
+export { default as Auth } from "./Auth";
